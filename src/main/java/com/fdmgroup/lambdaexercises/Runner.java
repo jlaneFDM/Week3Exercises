2f796425ebf04b8ef65dda58cf26a5ab00e8094c
John Lane <john.lane@fdmgroup.com>
2021-12-06 19:59:04 -0500

    completed to 1.4.3

--- src/main/java/com/fdmgroup/lambdaexercises/Runner.java
+++ src/main/java/com/fdmgroup/lambdaexercises/Runner.java
@@ -1,10 +1,13 @@
 package com.fdmgroup.lambdaexercises;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-
 
 public class Runner {
 
@@ -28,20 +31,17 @@ public class Runner {
 			System.out.println(getCustomerNameAndBalance.apply(t3));
 			
 		/*2*/
-			
 		Function<BankAccount, Double> getInterestDue = 
 				(account) -> account.getBalance() * (account.getInterestRate()/100);
 			System.out.println("Interest due: " + getInterestDue.apply(t2));
 			
 		/*3.*/
-			
 		Function<BankAccount, Double> returnZeroIfOverdrawn = 
 				(account) -> account.getBalance() * (account.getInterestRate()/100) > 0? 
 							account.getBalance() * (account.getInterestRate()/100) : 0;
 			System.out.println(returnZeroIfOverdrawn.apply(t8));//outputs 0
 			
 		/* 4.*/
-			
 		Predicate<BankAccount> determineIfCurrent = (account) -> account.getAccountType() == "current/checking";
 			System.out.println(determineIfCurrent.test(t1));
 			
@@ -50,7 +50,6 @@ public class Runner {
 			System.out.println("Overdrawn ? : " + determineIfOverdrawn.test(t5));
 			
 		/*6.*/
-		
 		BinaryOperator<BankAccount> getHigherBalance = 
 				(a1, a2) -> a1.getBalance() > a2.getBalance()? a1: a2;
 			System.out.println("Higher Balance between t3 & t4: " + getHigherBalance.apply(t3, t4));
@@ -67,6 +66,41 @@ public class Runner {
 				(account, deduction) -> account.setBalance(account.getBalance() - deduction);
 			deductFromBalance.accept(t1, 100);
 			System.out.println("Balance after user-defined deduction: " + t1.getBalance());	
+			
+		//1.3 Writing Lambdas For List Methods
+		//1	
+		List<BankAccount> accounts = new ArrayList<BankAccount>();
+		accounts.add(t1);
+		accounts.add(t2);
+		accounts.add(t3);
+		accounts.add(t4);
+		accounts.add(t5);
+		accounts.add(t6);
+		accounts.add(t7);
+		accounts.add(t8);
+		//2
+		accounts.forEach(a -> System.out.println(a.getAccountNumber() 
+				+ " " + a.getAccountHolder() + " " + a.getAccountType() + " " + a.getBalance()));
+		//3
+		accounts.forEach(a ->  a.setBalance(a.getBalance() -10));
+		//4
+		accounts.removeIf(a -> a.getBalance() < -500);
+		//5
+		accounts.removeIf(a -> a.getAccountType() == "salary");
+		
+		//1.4
+		//1
+		Comparator<BankAccount> getBiggerBalance = (a1, a2) -> a1.getBalance() > a2.getBalance()? 1:0;
+		Collections.sort(accounts, getBiggerBalance);
+		//2
+		Comparator<BankAccount> sortByAcctType = (b1, b2) -> b1.getAccountType().compareTo(b2.getAccountType());
+			Collections.sort(accounts, sortByAcctType);
+		//3
+		Comparator<BankAccount> sortByAcctNum = (c1, c2) -> c1.getAccountNumber() > c2.getAccountNumber()? 1:0;
+			Collections.sort(accounts,sortByAcctNum);
+		
+		
+		
 				
 	}
 
